2ce1677496009842fccf4ede83864a5c1e47fe59
Seither2k <kchindongo@outlook.com>
2025-06-09 20:36:34 +0200

    cypress additions
    added cypress files

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/* eslint-disable no-undef */
+describe('App E2E', () => {
+  it('loads page and fetches items', () => {
+    cy.visit('/');
+    cy.contains('Widgets');
+    cy.server();
+    cy.route('GET', '/api/widgets', [{id:1,name:'A'}]);
+    cy.get('.widget-item').should('have.length', 1);
+  });
+});
