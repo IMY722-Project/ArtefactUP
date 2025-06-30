8b8f9e8e7758ae76ef965992a7f51f862e088096
Tumi <u21452832@tuks.co.za>
2025-06-30 09:35:26 +0200

    fix: login

--- frontend/src/Admin/Admin.jsx
+++ frontend/src/Admin/Admin.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import {useAuth} from "react-oidc-context";
 import SideNav from "./SideNav";
-import {Outlet, useLocation, useNavigate} from "react-router";
+import {Outlet, useLocation} from "react-router";
 import "./Admin.css";
 
 export default function Admin() {
     const auth = useAuth();
-    const navigate = useNavigate();
     const location = useLocation();
 
     if (auth.error) {
@@ -19,6 +18,7 @@ export default function Admin() {
     }
 
     return (
+        auth.isAuthenticated &&
         <div className="flex h-screen w-screen overflow-hidden page-wrapper hide-scroll">
             <SideNav/>
             <div className="flex-1 overflow-auto my-4 mx-10 hide-scroll">
